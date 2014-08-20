db1f005f2d7f46d960a99c93734489d105f51517
SGeshev <wispa@abv.bg>
2014-08-20 09:42:30 +0300

    sound test

--- src/game/main.js
+++ src/game/main.js
@@ -7,6 +7,16 @@ game.addAsset('logo.png');
 game.addAsset('duck-hunt.png');
 game.addAsset('duckhunt_various_sheet.png');
 
+
+
+/*game.addAudio('sounds/Game_Over_1.mp3', 'over1');
+game.addAudio('sounds/Game_Over_2.mp3', 'over2');
+game.addAudio('sounds/Game_Start.mp3', 'gameStart');
+game.addAudio('sounds/Perfect_Round.mp3', 'perfect');
+game.addAudio('sounds/Shot_Duck.mp3', 'duckShot');
+game.addAudio('sounds/Stage_Clear.mp3', 'clear');
+game.addAudio('sounds/Title_Theme.mp3', 'title'); */
+
 game.SceneMenu = game.Scene.extend({
     backgroundColor: 0xb9bec7,
 
