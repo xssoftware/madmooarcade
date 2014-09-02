7aec081103d744da980894d8ec9c3ce0c69e5fb7
SGeshev <wispa@abv.bg>
2014-09-02 11:04:46 +0300

    title screen sound

--- src/game/main.js
+++ src/game/main.js
@@ -10,21 +10,16 @@ game.addAsset('duckhunt_various_sheet.png');
 game.addAudio('duck.ogg', 'duck');
 game.addAudio('Game_Start.mp3', 'gameStart');
 game.addAudio('dog bark.ogg', 'bark');
+game.addAudio('Title_Theme.ogg', 'title');
 
 
-/*game.addAudio('sounds/Game_Over_1.mp3', 'over1');
-game.addAudio('sounds/Game_Over_2.mp3', 'over2');
 
-game.addAudio('sounds/Perfect_Round.mp3', 'perfect');
-game.addAudio('sounds/Shot_Duck.mp3', 'duckShot');
-game.addAudio('sounds/Stage_Clear.mp3', 'clear');
-game.addAudio('sounds/Title_Theme.mp3', 'title'); */
 
 game.SceneMenu = game.Scene.extend({
     backgroundColor: 0xb9bec7,
 
     init: function() {
-		
+		game.audio.playSound('title');
 		var text = new game.PIXI.Text('Start Game');
 		text.interactive = true;
 		text.position = {x:150, y:150};
@@ -43,6 +38,7 @@ game.SceneGame = game.Scene.extend({
     backgroundColor: 0xb9bec7,
 
     init: function() {
+	    game.audio.stopSound('title');
 	    game.audio.playSound('gameStart',null, null, function(){
 		game.audio.playSound('bark');
 		});
