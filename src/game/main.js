6cc1d0d92cd8b93fb70c068cb498184053ed2a82
SGeshev <wispa@abv.bg>
2014-09-16 15:13:12 +0300

    scene 3 test

--- src/game/main.js
+++ src/game/main.js
@@ -8,6 +8,7 @@ game.addAsset('duck-hunt.png');
 game.addAsset('duckhunt_various_sheet.png');
 game.addAsset('duck-hunt-start.png');
 game.addAsset('fly away.png');
+game.addAsset('scene3.png');
 
 game.addAudio('duck.ogg', 'duck2');
 game.addAudio('Game_Start.mp3', 'gameStart');
@@ -31,7 +32,7 @@ game.SceneMenu = game.Scene.extend({
 		var background2 = new game.Sprite('duck-hunt-start.png').addTo(this.stage);
 		background2.interactive = true;
 		background2.click = function(){
-		game.system.setScene(game.SceneGame);
+		game.system.setScene(game.SceneScores);
 		};
 		//var text = new game.PIXI.Text('Start Game');
 		//text.interactive = true;
@@ -75,6 +76,22 @@ game.SceneScores = game.Scene.extend({
     backgroundColor: 0xb9bec7,
 
     init: function() {
+		var background3 = new game.Sprite('scene3.png').addTo(this.stage);
+	var newgame = new game.PIXI.Text('New Game');
+	newgame.interactive = true;
+	newgame.position = {x:374, y:120};
+	
+	var highScores = new game.PIXI.Text('High Scores');
+	highScores.interactive.true;
+	highScores.position = {x:374, y:160};
+	
+	newgame.click = function()
+	{
+	   game.system.setScene(game.SceneGame);
+	};
+	
+     this.stage.addChild(newgame);
+	 this.stage.addChild(highScores);
      
 	
     }
